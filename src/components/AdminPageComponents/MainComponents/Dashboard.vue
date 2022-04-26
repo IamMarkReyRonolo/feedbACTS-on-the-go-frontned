8556169b3b98630b9f06ec79536c7ea8924439d1
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2022-04-26 21:30:50 +0800

    modify data table design

--- src/components/AdminPageComponents/MainComponents/Dashboard.vue
+++ src/components/AdminPageComponents/MainComponents/Dashboard.vue
@@ -218,9 +218,9 @@
 
 	.tableCon {
 		height: 500px;
-		/* background-color: white;
+		background-color: white;
 		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-		border-radius: 20px; */
+		border-radius: 20px;
 		padding: 20px;
 	}
 
