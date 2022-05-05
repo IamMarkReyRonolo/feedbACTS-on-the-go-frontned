6f92a2661fa89a946cc306cd7dad1d713d446a8a
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2022-05-06 00:24:44 +0800

    add report title and report details to generate report

--- src/components/AdminPageComponents/PopUpComponents/GenerateReportDialog.vue
+++ src/components/AdminPageComponents/PopUpComponents/GenerateReportDialog.vue
@@ -1,7 +1,27 @@
 <template>
 	<v-dialog v-model="clickGenerateReport" max-width="350" persistent>
-		<v-card>
+		<v-card v-if="clickGenerateReport">
 			<v-card-title class="dialogTitle"> Generate Report </v-card-title>
+			<div class="detail">
+				<v-text-field
+					label="Report Title"
+					outlined
+					color="#5aa67a"
+					dense
+					v-model="title"
+					:rules="[rules.required]"
+				></v-text-field>
+			</div>
+			<div class="detail">
+				<v-textarea
+					label="Report Details"
+					outlined
+					color="#5aa67a"
+					dense
+					no-resize
+					v-model="details"
+				></v-textarea>
+			</div>
 
 			<div class="date">
 				<div class="label">From</div>
@@ -20,8 +40,8 @@
 				<v-btn
 					color="green darken-1"
 					@click="generateReport"
-					:dark="dates.length == 2"
-					:disabled="dates.length != 2"
+					:dark="validate"
+					:disabled="!validate"
 					:loading="loading"
 				>
 					Generate Report
@@ -39,7 +59,12 @@
 		data() {
 			return {
 				loading: false,
+				title: "",
+				details: "",
 				dates: [],
+				rules: {
+					required: (value) => !!value || "Required.",
+				},
 			};
 		},
 
@@ -54,7 +79,19 @@
 			},
 			cancel() {
 				this.dates = [];
+				this.details = "";
+				this.title = "";
 				this.$emit("cancelReport");
+			},
+		},
+
+		computed: {
+			validate: function () {
+				if (this.dates.length == 2 && this.title.trim()) {
+					return true;
+				}
+
+				return false;
 			},
 		},
 	};
@@ -72,7 +109,7 @@
 
 	.date {
 		text-align: left;
-		margin: 10px 40px;
+		margin: 10px 20px;
 	}
 
 	.date .label {
@@ -88,5 +125,9 @@
 		width: 100%;
 		font-size: 18px;
 		border-radius: 10px;
+	}
+
+	.detail {
+		margin: 0px 20px;
 	}
 </style>
